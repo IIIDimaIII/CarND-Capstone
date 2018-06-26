665f388dca9ee660aca290e9cf9d9623ca626bf3
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-06-26 11:41:38 +0800

    test

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -83,11 +83,13 @@ class TLDetector(object):
 
         # --------------------------------------------------
         # Getting image from camera
-        ctime = timeit.default_timer()
-        if self.last_time_image_cb_called - ctime > 100:
-            with open("image_dump", "w") as f:
-                f.write(msg)
-            self.last_time_image_cb_called = ctime           
+        with open("~/work/CarND-Capstone/test_output.txt", "w") as f:
+            f.write(msg)
+        #ctime = timeit.default_timer()
+        #if self.last_time_image_cb_called - ctime > 100:
+        #    with open("image_dump", "w") as f:
+        #        f.write(msg)
+        #    self.last_time_image_cb_called = ctime           
         # --------------------------------------------------
 
         '''
