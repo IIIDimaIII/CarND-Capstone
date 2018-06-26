3a772d92801833a87c401809564d3346a084a80c
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-06-26 12:04:01 +0800

    update

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -89,7 +89,7 @@ class TLDetector(object):
             im_width = self.camera_image.width
             im_data = self.camera_image.data
             img = np.array(im_data)
-            f.write(img)
+            f.write(im_data)
         #ctime = timeit.default_timer()
         #if self.last_time_image_cb_called - ctime > 100:
         #    with open("image_dump", "w") as f:
