bb07e77918d6dd57860ee2e0de0e2403d040d487
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-06-27 16:02:52 +0800

    updated

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -81,21 +81,6 @@ class TLDetector(object):
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
 
-        # --------------------------------------------------
-        # Getting image from camera
-        #with open("/home/student/work/CarND-Capstone/test_output.txt", "w") as f:
-        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-        #rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)        
-        #cv2.imwrite('camera-{}.png'.format(timeit.default_timer()), cv_image)
-        #with open("test_output.png", "w") as f:            
-        #    f.write(cv_image)            
-        #ctime = timeit.default_timer()
-        #if self.last_time_image_cb_called - ctime > 100:
-        #    with open("image_dump", "w") as f:
-        #        f.write(msg)
-        #    self.last_time_image_cb_called = ctime           
-        # --------------------------------------------------
-
         '''
         Publish upcoming red lights at camera frequency.
         Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
